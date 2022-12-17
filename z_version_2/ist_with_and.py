3dff25e690eb2ba351525ee486b7ff7f7484c49e
Muhammad ijaz <75518471+Muhammad-ijazCRT@users.noreply.github.com>
2022-12-17 09:16:34 +0500

    Add files via upload

---
+++ z_version_2/ist_with_and.py
@@ -0,0 +1,12 @@
+test_list = [0,1,1,0,0]
+
+res = [ test_list[1], test_list[2] ]
+sec = [ test_list[0], test_list[3], test_list[4] ]
+
+if res ==[1,1] and sec == [0,0,0]:
+    print('yes')
+else:
+    print('No')
+
+# if res == '1' and sec == 0:
+#     print('alhamdulillah')
